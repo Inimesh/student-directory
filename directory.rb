b145f437fc31b1b903b2e4b57d24efaa56ff25e7
Inimesh <ijmeshoulam@gmail.com>
2021-11-13 11:41:59 +0000

    adding 'country of birth'credentials

--- directory.rb
+++ directory.rb
@@ -14,8 +14,8 @@
 # ]
 
 def input_students
-    puts "Please enter the names of the students, then the cohort."
-    puts "To finish, just hit return three times."
+    puts "Please enter student credentials as prompted."
+    puts "To finish, enter blank credentials."
     # create an empty array
     students = []
     # get the first name
@@ -23,16 +23,20 @@ def input_students
     name = gets.chomp
     puts "Please enter cohort:"
     cohort = gets.chomp
+    puts "Please enter country of birth:"
+    cob = gets.chomp
     # while the name is not empty, repeat this code
     until name.empty?
         # add the student hash to the array
-        students << {name: name, cohort: cohort}
+        students << {name: name, cohort: cohort, cob: cob}
         puts "Now we have #{students.count} students"
         # get another name and cohort from the user
         puts "Please enter name:"
         name = gets.chomp
         puts "Please enter cohort:"
         cohort = gets.chomp
+        puts "Please enter country of birth:"
+        cob = gets.chomp
     end
     students 
 end
@@ -44,7 +48,7 @@ end
 
 def print(students)
     students.each_with_index { |student, index|
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) (Contry of birth: #{student[:cob]})"
     }
 end
 
