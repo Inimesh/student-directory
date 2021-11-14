344c50a1e7c7db391bb1f53470073953fa687414
Inimesh <ijmeshoulam@gmail.com>
2021-11-14 11:59:03 +0000

    Refactoring interactive menu code

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
+@students = []
 
-def input_students
+def input_students()
     puts "Please enter student credentials as prompted."
     puts "To finish, enter blank credentials."
-    # create an empty array
-    students = []
     # get the first name
     puts "Please enter name:"
     name = gets.chomp
@@ -21,8 +20,8 @@ def input_students
     # while the name is not empty, repeat this code
     until name.empty?
         # add the student hash to the array
-        students << {name: name, cohort: cohort, cob: cob}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: cohort, cob: cob}
+        puts "Now we have #{@students.count} students"
         # get another name and cohort from the user
         puts "Please enter name:"
         name = gets.chomp
@@ -31,32 +30,31 @@ def input_students
         puts "Please enter country of birth:"
         cob = gets.chomp
     end
-    students 
 end
 
-def print_header
+def print_header()
     puts "The students of Villains Academy"
     puts  "-------------------------------"
 end
 
-def print_n(students)
+def print_students_list()
     puts "All student listing"
-    students.each_with_index { |student, index|
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) (Contry of birth: #{student[:cob]})"
+    @students.each_with_index { |student, index|
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) (Country of birth: #{student[:cob]})"
     }
     print "\n"
 end
 
-def print_by_cohort(students)
+def print_by_cohort()
     puts "Students organised by cohort:"
     students_by_cohort = {}
     # Initialize hash of cohort keys, empty array values
-    students.each { |student| 
+    @students.each { |student| 
         students_by_cohort[student[:cohort]] = []
     }
 
     # append student details to array value associated with cohort key
-    students.each { |student| 
+    @students.each { |student| 
         students_by_cohort[student[:cohort]] << student
     }
 
@@ -70,33 +68,39 @@ def print_by_cohort(students)
     }
 end
 
-def print_footer(names)
-    puts names.count > 1 ? "Overall, we have #{names.count} great students" : "Overall, we have #{names.count} great student"
+def print_footer()
+    puts @students.count > 1 ? "Overall, we have #{@students.count} great students" : "Overall, we have #{@students.count} great student"
 end
 
+def print_menu()
+    puts "1. Input the students"
+    puts "2. show the students"
+    puts "9. Exit"
+end
 
-def interactive_menu
-    students = []
+def show_students()
+    print_header()
+    print_students_list()
+    print_footer()
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students()
+    when "2"
+        show_students()
+    when "9"
+        exit
+    else
+        puts "I don't know what you mean, try again."
+    end
+end
+
+def interactive_menu()
     loop do
-        # 1. print the menu and ask the user what to do
-        puts "1. Input the students"
-        puts "2. show the students"
-        puts "9. Exit"
-        # 2. read the input and save it into a variable
-        selection = gets.chomp
-        # 3. do what the user has asked
-        case selection
-        when "1"
-            students = input_students
-        when "2"
-            print_header 
-            print_n(students)
-            print_footer(students)
-        when "9"
-            exit # this will terminate program
-        else 
-            puts "I don't know what you meant, try again."
-        end
+        print_menu()
+        process(gets.chomp)
     end
 end
 
